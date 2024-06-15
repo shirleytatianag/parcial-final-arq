110504c6547eea9ad8b1e7d61ef889a449c73f24
Shirley Tatiana García <shirleygarcia@wposs.con>
2024-06-15 08:41:53 -0500

    Ajustes interceptor

--- src/app/core/interceptor/auth-interceptor/auth.interceptor.ts
+++ src/app/core/interceptor/auth-interceptor/auth.interceptor.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs';
 import {AlertService} from "@app/core/services/alert.service";
 import {StorageService} from "@app/core/services/storage.service";
 import {AuthService} from "@app/modules/auth/services/auth.service";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -12,17 +13,27 @@ export class AuthInterceptor implements HttpInterceptor {
     private _storage: StorageService,
     private _auth: AuthService,
     private _alertService: AlertService,
+    private _router: Router,
   ) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const access_token = this._storage.getItem<string>('access_token');
 
+    if (request.url === 'https://api.escuelajs.co/api/v1/auth/login') {
+      return next.handle(request);
+    }
+
     if (access_token) {
       request = AuthInterceptor.addTokenHeader(request, access_token);
     } else {
-      // this._alertService.warning('Tiempo expirado, vuelve a iniciar sesión');
-      // this._auth.logout();
+      this._auth.logout();
+      this._alertService.warning('Tiempo expirado, vuelve a iniciar sesión');
+      this._router.navigateByUrl('/').then(
+        () => {
+          location.reload();
+        }
+      );
     }
     return next.handle(request);
   }
